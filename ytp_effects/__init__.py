f23bf5e1201b8a3e3f1fb875cc01ab4ae784eb07
Karl Gylleus <gylleus@kth.se>
2020-06-27 18:33:24 +0900

    Add more exported functions

--- ytp_effects/__init__.py
+++ ytp_effects/__init__.py
@@ -1,1 +1,3 @@
 import ytp_effects.image
+import ytp_effects.audio
+import ytp_effects.video
---
+++ ytp_effects/audio/__init__.py
@@ -0,0 +1,3 @@
+import ytp_effects.audio.echo
+import ytp_effects.audio.pitch
+import ytp_effects.audio.space
